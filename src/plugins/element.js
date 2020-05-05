cf77670f5eeb998ba242ade9ab425d45cb439dba
coffee <87863496@qq.com>
2020-05-05 12:13:59 +0800

    登录退出功能完成

---
+++ src/plugins/element.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+//导入弹框提示组件
+import {Button, Form, FormItem, Input, Message} from 'element-ui'
+
+Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.prototype.$message = Message
